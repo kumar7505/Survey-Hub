b54ea2f51b1bd5e230e3e3ea1ccfeb30a6b3db13
kumar7505 <kumarpers7@gmail.com>
2024-12-01 10:58:56 +0530

    feat: displaying the poll

--- survey/poll/views.py
+++ survey/poll/views.py
@@ -3,5 +3,7 @@ from .models import Question, Choice
 # Create your views here.
 
 def index(request):
-    return render(request, 'poll/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'poll/index.html', context)
     # return HttpResponse('<h1>Hello</h1>')
