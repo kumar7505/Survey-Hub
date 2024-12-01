111b57b764c5873394c6f9e4d28531321e9ed02f
kumar7505 <kumarpers7@gmail.com>
2024-12-01 19:40:12 +0530

    feat: Home Page

--- survey/poll/views.py
+++ survey/poll/views.py
@@ -26,6 +26,7 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        print(selected_choice)
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
