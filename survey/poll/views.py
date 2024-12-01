9fb2842300151432e46cf2cec5ff39939de35c58
kumar7505 <kumarpers7@gmail.com>
2024-12-01 20:01:43 +0530

    fix: vote count

--- survey/poll/views.py
+++ survey/poll/views.py
@@ -26,7 +26,7 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-        print(selected_choice)
+        print("Request POST data:", request.POST)
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
@@ -39,4 +39,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
