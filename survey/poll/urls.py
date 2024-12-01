5430b298ec0cb0e455ecabb54b3b60aaac10b165
kumar7505 <kumarpers7@gmail.com>
2024-12-01 23:21:01 +0530

    feat: AJAX call

--- survey/poll/urls.py
+++ survey/poll/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('<int:question_id>', views.detail, name='detail'),
     path('<int:question_id>/results', views.results, name='results'),
     path('<int:question_id>/vote', views.vote, name='vote'),
-    path('resultdata/<str:obj>/', views.resultData, name="resultdata")
+    path('resultsdata/<str:obj>/', views.resultData, name="resultsdata")
 ]
