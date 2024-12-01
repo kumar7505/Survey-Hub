c6265523013669727b18bacf8097113dba630262
kumar7505 <kumarpers7@gmail.com>
2024-12-01 10:40:21 +0530

    feat: templates

--- survey/poll/views.py
+++ survey/poll/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Question, Choice
 # Create your views here.
 
 def index(request):
     return render(request, 'poll/index.html')
+    # return HttpResponse('<h1>Hello</h1>')
